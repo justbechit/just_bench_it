bff8dcf042fc3624195acb077aa5a27117891a43
370025263 <370025263@qq.com>
2024-08-01 17:45:58 +0800

    Bump version: 0.1.5 → 0.1.6

--- just_bench_it/__init__.py
+++ just_bench_it/__init__.py
@@ -2,9 +2,7 @@ from just_bench_it.benchmarker import benchmark
 from just_bench_it.utils import print_results
 import os
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 def set_github_token(token):
     os.environ['GITHUB_TOKEN'] = token
 
-from . import _version
-__version__ = _version.get_versions()['version']
